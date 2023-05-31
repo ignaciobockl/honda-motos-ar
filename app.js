73ea3265e4fe36474816c9d2409e553834ed6056
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-05-30 21:26:39 -0300

    seo

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('Hello console')
+console.log('Hola consola');
+
+const btnBuscar = document.getElementById('btn-nav-buscar');
+
+btnBuscar.addEventListener('click', () => {
+  // TODO: funcionalidad pendiente
+  console.log('Botón "Buscar" presionado');
+});
